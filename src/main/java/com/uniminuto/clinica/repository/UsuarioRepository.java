09a0f2f0a9760b34047ff261d356275859b51549
samuel <samusplay08@gmail.com>
2025-09-01 19:55:43 -0500

    Configuracion inicial

---
+++ src/main/java/com/uniminuto/clinica/repository/UsuarioRepository.java
@@ -0,0 +1,19 @@
+package com.uniminuto.clinica.repository;
+
+import com.uniminuto.clinica.entity.Usuario;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author lmora
+ */
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String nombre);
+
+    List<Usuario> findByRol(String rol);
+
+}
---
+++ src/main/java/com/uniminuto/clinica/service/UsuarioService.java
@@ -0,0 +1,18 @@
+package com.uniminuto.clinica.service;
+
+import com.uniminuto.clinica.entity.Usuario;
+import java.util.List;
+import org.apache.coyote.BadRequestException;
+
+/**
+ *
+ * @author lmora
+ */
+public interface UsuarioService {
+    List<Usuario> listarTodosLosUsuarios();
+
+    Usuario buscarUsuarioPorNombre(String nombre)
+            throws BadRequestException;
+
+    List<Usuario> buscarPorRol(String role);
+}
